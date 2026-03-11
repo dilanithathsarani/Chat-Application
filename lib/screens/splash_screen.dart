d594684231b7ca75235b9585690e0b3f80424c54
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-03-11 22:12:50 +0530

    creating splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -11,10 +12,18 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: SafeArea(child: SizedBox(
+        height: 50,
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Text('From SUSL')),
+      )),
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
+          LottieBuilder.asset('assets/lottie/chat.json'),
             Text(
               'My Chat App',
               style: TextStyle(
