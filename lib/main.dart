027a8bb86f16149b58ba9faa8b5c38314c8fa95b
dilanithathsarani <dilanithathsarani2003@gmail.com>
2026-08-07 17:22:50 +0530

    integrate firebase_messaging and implement push notification permission request in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:chat_application/providers/chat_provider.dart';
 import 'package:chat_application/providers/user_provider.dart';
 import 'package:chat_application/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await requestPermission();
   runApp(
     MultiProvider(
       providers: [
@@ -18,6 +21,22 @@ void main() async {
       child: MyApp(),
     ),
   );
+}
+
+Future<void> requestPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+NotificationSettings settings = await messaging.requestPermission(
+  alert: true,
+  announcement: false,
+  badge: true,
+  carPlay: false,
+  criticalAlert: false,
+  provisional: false,
+  sound: true,
+);
+
+Logger().f('User granted permission: ${settings.authorizationStatus}');
 }
 
 class MyApp extends StatelessWidget {
